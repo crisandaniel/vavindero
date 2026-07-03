989f24a8f69e0e192d561abbcd232c51d57cdbbb
Daniel Crisan <crisandaniel@gmail.com>
2026-07-03 11:41:09 +0300

    MOved site to root folder

--- web/wp-content/plugins/hostinger-ai-assistant/gutenberg-block/dist/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'e68024e655813ed60755');
--- web/wp-content/plugins/hostinger-reach/frontend/dist/blocks/subscription-view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '44574f9cdd7c30701b79');
