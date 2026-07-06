8333f20fa562adbb77248a8cd0e045389d00e4c6
Daniel Crisan <crisandaniel@gmail.com>
2026-07-06 18:10:18 +0300

    Add Astra and Neve themes for the main site
    Astra is now active on the main site (vavinde.ro) for a more
    business-like presentation page look; Neve installed alongside as an
    alternative, network-enabled but inactive.

---
+++ wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v4.2.7',
+        'version' => '4.2.7.0',
+        'reference' => '5d07e1be8edb9839f39d47b86a48efa9b92dfdbf',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v4.2.7',
+            'version' => '4.2.7.0',
+            'reference' => '5d07e1be8edb9839f39d47b86a48efa9b92dfdbf',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.54',
+            'version' => '3.3.54.0',
+            'reference' => '095c2d0f1388af0b0196c492a7f79e2fd092dab1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '68c93e1dc8f6e438f93e81ebdf632e6243e43e0f',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
